e498bf8955ea68fb36d28bf87bdcc8100a1444ad
Johannes Loewe <cortinarius.orellanus@gmail.com>
2016-01-22 23:22:33 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	var xCount = 16;
-	var yCount = 8;
+	var xCount = prompt("x");
+	var yCount = xCount*0.75;
 	for(i=0;i<yCount;i++) {
 		$("tbody").append("<tr id='column"+i+"'>");
 		for(j=0;j<xCount;j++) {
@@ -11,10 +11,21 @@ $(document).ready(function(){
 	$("td").css("width:"+(100/16)+"%;");
 	$("tr").css("height:"+(100/16)+"%;");
 
-	$(".tile").click(function(){
-	$(this).css("background-color","#000000");
+	$(".tile").hover(function(){
+		$(this).css("opacity",""+(parseFloat($(this).css("opacity"))+0.1));
 	});
 
 });
+
+function randomColor(){
+	var color = [];
+	var hex = ["A","B","C","D","E","F",1,2,3,4,5,6,7,8,9];
+	for(i=0;i<6;i++) {
+		color[i] = hex[Math.floor(Math.random()*15)];
+	}
+	return "#"+(color.toString()).replace(/\,/g,"");
+};
+
+
 
 
